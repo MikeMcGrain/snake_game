dc0fab6b6951932ddef96bef9c2f9d340a17d6b1
Mike McGrain <mcgrain.mike@gmail.com>
2020-02-13 20:13:45 -0600

    snake and mouse as objects

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const canvas = document.getElementById("canvas")
 const canvasContext = canvas.getContext("2d")
 
-const snakeSize = 20
-let snakeX = canvas.width / 2
-let snakeY = canvas.height / 2
-let snakeSpeed = 20
-let snakeDirection = 0
+let snake = {
+  size: 25,
+  speed: 25,
+  x: canvas.width / 2,
+  y: canvas.height / 2,
+  direction: 0
+}
 
-const mouseSize = 20
-let mouseX = getRandomNumber(0, canvas.width-mouseSize)
-let mouseY = getRandomNumber(0, canvas.height-mouseSize)
+let mouse = {
+  size: 25,
+  x: 200,
+  y: 200
+}
 
 let score = 0
 document.getElementById("score").innerText = `Score: ${score}`
@@ -17,82 +21,85 @@ document.getElementById("score").innerText = `Score: ${score}`
 window.addEventListener("load", function() {
   const framesPerSec = 15
   setInterval(() => {
-    checkSnakePos()
-    draw()
+    drawCanvas()
+    drawMouse()
+    drawSnake()
+    checkSnakePosition()
     moveSnake()
   }, 1000 / framesPerSec)
 
   document.addEventListener("keydown", function(e) {
     if (e.keyCode === 37 || e.keyCode === 38 || e.keyCode === 39 || e.keyCode === 40) {
-      snakeDirection = e.keyCode
+      snake.direction = e.keyCode
     }
   })
 })
 
-function checkSnakePos() {
-  if (snakeX == mouseX && snakeY == mouseY) {
-    mouseX = getRandomNumber(0, canvas.width-mouseSize)
-    mouseY = getRandomNumber(0, canvas.height-mouseSize)
+function drawCanvas() {
+  drawRectangle(0, 0, canvas.width, canvas.height, "black")
+}
+
+function drawMouse() {
+  drawRectangle(mouse.x, mouse.y, mouse.size, mouse.size, "gray")
+}
+
+function drawSnake() {
+  drawRectangle(snake.x, snake.y, snake.size, snake.size, "green")
+}
+
+function drawRectangle(leftX, topY, width, height, color) {
+  canvasContext.fillStyle = color
+  canvasContext.fillRect(leftX, topY, width, height, color)
+}
+
+function checkSnakePosition() {
+  if (snake.x == mouse.x && snake.y == mouse.y) {
+    mouse.x = getRandomNumber(0, canvas.width-mouse.size)
+    mouse.y = getRandomNumber(0, canvas.height-mouse.size)
     document.getElementById("score").innerText = `Score: ${++score}`
   }
 
-  if (snakeX < 0 || snakeX >= (canvas.width) || snakeY < 0 || snakeY >= (canvas.height)) {
+  if (snake.x < 0 || snake.x >= (canvas.width) || snake.y < 0 || snake.y >= (canvas.height)) {
     alert(`Game Over -- Final Score: ${score}`)
-    snakeX = canvas.width / 2
-    snakeY = canvas.height / 2
-    mouseX = getRandomNumber(0, canvas.width-mouseSize)
-    mouseY = getRandomNumber(0, canvas.height-mouseSize)
-    snakeDirection = 0
+    snake.x = canvas.width / 2
+    snake.y = canvas.height / 2
+    mouse.x = getRandomNumber(0, canvas.width-mouse.size)
+    mouse.y = getRandomNumber(0, canvas.height-mouse.size)
+    snake.direction = 0
     score = 0
     document.getElementById("score").innerText = `Score: ${score}`
   }
 }
 
-function draw() {
-  // draw canvas
-  drawRect(0, 0, canvas.width, canvas.height, "black")
-
-  // draw snake
-  drawRect(snakeX, snakeY, snakeSize, snakeSize, "green")
-
-  // draw mouse
-  drawRect(mouseX, mouseY, mouseSize, mouseSize, "gray")
-}
-
-function drawRect(leftX, topY, width, height, color) {
-  canvasContext.fillStyle = color
-  canvasContext.fillRect(leftX, topY, width, height, color)
+function getRandomNumber(min, max) {
+  return Math.round((Math.random()*(max-min)+min)/snake.speed)*snake.speed
 }
 
 function moveSnake() {
-  switch (snakeDirection) {
+  switch (snake.direction) {
     case 37: // left-key
       do {
-        snakeX -= snakeSpeed
+        snake.x -= snake.speed
         break
       } while (keyNum == 37)
       break
     case 38: // up-key
       do {
-        snakeY -= snakeSpeed
+        snake.y -= snake.speed
         break
       } while (keyNum == 38)
       break
     case 39: // right-key
       do {
-        snakeX += snakeSpeed
+        snake.x += snake.speed
         break
       } while (keyNum == 39)
       break
     case 40: // down-key
       do {
-        snakeY += snakeSpeed
+        snake.y += snake.speed
         break
       } while (keyNum == 40)
       break
   }
-}
-
-function getRandomNumber(min, max) {
-  return Math.round((Math.random()*(max-min)+min)/20)*20
 }
